05a33f088a03db96dfc7f2c7deca1b24ab3835fd
elvancitradynata-dotcom <elvancitradynata@gmail.com>
2026-06-04 19:57:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6709,7 +6709,6 @@ YDL_OPTIONS = {
     "no_warnings": True,
     "default_search": "ytsearch",
     "source_address": "0.0.0.0",
-    "postprocessors": [{"key": "FFmpegExtractAudio", "preferredcodec": "opus"}],
 }
 FFMPEG_OPTIONS = {
     "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
